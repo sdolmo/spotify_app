d37402b3f83dda2c8cc3f7f1db737c5a47f761e4
Sylvia <sylviad1221@gmail.com>
2016-10-12 01:23:11 -0400

    add body-parser and access request middleware

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser"); //Do blog post on body parse and what is middleware
 var request = require("request");
 
-app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true})); //what does this do?
+app.set("view engine", "ejs"); //sets all view extensions to ejs
 
-// Provide form to search for artists
+// Provide form to search for artists in landing.ejs and render in root route
 app.get("/", function(req, res){
   res.render("landing");
 });
 
-// render search results from Spotify API
-app.get("results", function(req, res){
+// render search results from Spotify API in results.ejs
+app.get("/results", function(req, res){
+  var query = req.query.search;
+  var url = "https://api.spotify.com/v1/artists";
+
+  res.render("results", {query: query});
 })
 
 app.listen(8000, function(){
